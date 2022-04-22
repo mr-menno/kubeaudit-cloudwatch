0a41d8d15056284a02e63a3ac2ff393aeeab32ba
menno <menno@lacework.com>
2022-04-21 17:06:53 -0700

    removed filter

--- src/CloudWatchShipper.js
+++ src/CloudWatchShipper.js
@@ -62,7 +62,7 @@ class CloudWatchShipper {
       this.nextSequenceToken = res.uploadSequenceToken || null;
     }
     //262144 is cloud watch event size limit
-    events = events.filter(e=>e.length<262144).map(e=>({
+    events = events.map(e=>({
       message: e,
       timestamp: new Date().getTime()
     }));
